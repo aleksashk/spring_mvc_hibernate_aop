ca68f71fe59db5747f1890af18e1c9b4855c0e65
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-11-05 22:29:33 +0300

    add layer Controller

---
+++ src/main/java/com/aleksandrphilimonov/spring/mvc_hibernate_aop/controller/MyController.java
@@ -0,0 +1,26 @@
+package com.aleksandrphilimonov.spring.mvc_hibernate_aop.controller;
+
+import com.aleksandrphilimonov.spring.mvc_hibernate_aop.dao.EmployeeDAO;
+import com.aleksandrphilimonov.spring.mvc_hibernate_aop.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+public class MyController {
+    @Autowired
+    private EmployeeDAO employeeDAO;
+
+    @RequestMapping("/")
+    public String showAllEmployees(Model model) {
+
+        List<Employee> allEmployees = employeeDAO.getAllEmployees();
+        model.addAttribute("allEmps", allEmployees);
+
+        return "all-employees";
+
+    }
+}
